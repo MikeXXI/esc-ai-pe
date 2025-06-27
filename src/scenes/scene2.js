30a3b341a1d8b350a6a7576251f5941ce3a01bd6
Mike <mickael.djegherif@outlook.fr>
2025-06-27 17:19:30 +0200

    scene2 fonctionnelle

--- src/scenes/scene2.js
+++ src/scenes/scene2.js
@@ -4,47 +4,49 @@ import { addHand } from '../objects/addhand.js';
 import { createScene3 } from './scene3.js';
 import { switchScene } from '../main.js';
 
-export function createScene2(engine, canvas) {
+export async function createScene2(engine, canvas) {
   const scene = new BABYLON.Scene(engine);
-  const camera2 = new BABYLON.ArcRotateCamera("Camera2", Math.PI / 2, Math.PI / 4, 5, BABYLON.Vector3.Zero(), scene);
+  const camera2 = new BABYLON.ArcRotateCamera("Camera2", 0, 0, 5, BABYLON.Vector3.Zero(), scene);
   camera2.attachControl(canvas, true);
 
-  const light = new BABYLON.HemisphericLight("Light2", new BABYLON.Vector3(1, 1, 0), scene);
+  const light = new BABYLON.HemisphericLight("Light2", new BABYLON.Vector3(0, 1, 0), scene);
   light.intensity = 0.7;
 
   // Ajout des mains (appel à la fonction importée)
   addHand(scene, camera2);  
 
-  BABYLON.SceneLoader.Append(
-    "/models/",
-    "env_egypt_map.glb",
-    scene,
-    function () {
-      console.log("Scene égyptienne chargée !");
-    },
-    null,
-    function (scene, message, exception) {
-      console.error("Erreur chargement GLB :", message, exception);
-    }
+
+  const cubeImport = await BABYLON.SceneLoader.ImportMeshAsync(
+    "",            // importer tous les meshess
+    "/models/",    // dossier
+    "cube_demo.glb", // nom du fichier
+    scene
   );
+  const cubeMeshes = cubeImport.meshes;
+  cubeMeshes.forEach(mesh => {
+    mesh.position = new BABYLON.Vector3(0, 0, 0);
+    mesh.checkCollisions = true;
+    mesh.isVisible = true;
+    if (mesh.name !== "__root__") {
+      mesh.actionManager = new BABYLON.ActionManager(scene);
+      mesh.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function () {
+          switchScene(createScene3);
+        })
+      );
+    }
 
+  });
 
-  // Cube interactif au centre
-    const cube2 = BABYLON.MeshBuilder.CreateBox("cube2", { size: 1 }, scene);
-    cube2.position = new BABYLON.Vector3(0, 0.5, 0);
-    const cubeMat = new BABYLON.StandardMaterial("cubeMat2", scene);
-    cubeMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
-    cube2.material = cubeMat;
-  
-    // Activer les collisions du cube
-    //cube.checkCollisions = true;
-    cube2.actionManager = new BABYLON.ActionManager(scene);
-    cube2.actionManager.registerAction(
-      new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function () {
-        switchScene(createScene3);
-      })
-    );
-  
+  const cube2 = BABYLON.MeshBuilder.CreateBox("cube2", { size: 1.5 }, scene);
+  cube2.position = new BABYLON.Vector3(2.5, 2, 0);
+  const cubeMat = new BABYLON.StandardMaterial("cubeMat2", scene);
+  cubeMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
+  cube2.material = cubeMat;
+
+  // Centrer la caméra sur le cube
+  camera2.setTarget(new BABYLON.Vector3(2.5, 2, 0));
+
 
   return scene;
 }
